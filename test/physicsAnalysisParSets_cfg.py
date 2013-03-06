2d4ac9b06d85b0458bd3e80d479c78b1c227b5b0
vischia <>
2013-03-06 16:30:11 +0000

    pileups

--- test/physicsAnalysisParSets_cfg.py
+++ test/physicsAnalysisParSets_cfg.py
@@ -10,8 +10,9 @@ PhysicsAnalysisParSet = cms.PSet(
     doWPlusJetsAnalysis = cms.bool(False),
     inputArea = cms.string("/lustre/ncg.ingrid.pt/cmslocal/samples/CMSSW_5_3_7_patch4/"),
     outputArea = cms.string("/lustre/ncg.ingrid.pt/cmslocal/samples/CMSSW_5_3_7_patch4/chiggs/"),
-    puFileName = cms.string("/exper-sw/cmst3/cmssw/users/vischia/chiggs/CMSSW_5_3_7_patch4/src/LIP/TopTaus/data/pileup/MyDataPileupHistogram_All_73500.root")
-    #; //MyDataPileupHistogram_All_70500.root");//MyDataPileupHistogram_All_73500.root");
+    #puFileName = cms.string("/exper-sw/cmst3/cmssw/users/vischia/chiggs/CMSSW_5_3_7_patch4/src/LIP/TopTaus/data/pileup/MyDataPileupHistogram_All_69400.root")
+    puFileName = cms.string("/exper-sw/cmst3/cmssw/users/vischia/chiggs/CMSSW_5_3_7_patch4/src/LIP/TopTaus/data/pileup/MyDataPileupHistogram_All_70500.root")
+    #puFileName = cms.string("/exper-sw/cmst3/cmssw/users/vischia/chiggs/CMSSW_5_3_7_patch4/src/LIP/TopTaus/data/pileup/MyDataPileupHistogram_All_73500.root")
     )
 
 
