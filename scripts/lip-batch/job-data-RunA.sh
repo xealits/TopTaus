45fea6b67fea87ca9fc593879055d5c029d82f40
Pietro Vischia <pietro.vischia@gmail.com>
2013-08-20 17:20:54 +0100

    Rationalize lip batch infrastructure for data
    Enabled runtime parameters for qsub data scripts

--- scripts/lip-batch/job-data-RunA.sh
+++ scripts/lip-batch/job-data-RunA.sh
@@ -2,4 +2,4 @@
 
 . $LIPCMS_BASE/scripts/lip-batch/run-batch.sh
 
-physicsAnalysis $LIPCMS_BASE/test/physicsAnalysisParSets_cfg.py data_muonA
+physicsAnalysis $LIPCMS_BASE/test/physicsAnalysisParSets_cfg.py data_muonA_${1}
--- scripts/lip-batch/job-data-RunC2_7.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-. $LIPCMS_BASE/scripts/lip-batch/run-batch.sh
-
-physicsAnalysis $LIPCMS_BASE/test/physicsAnalysisParSets_cfg.py data_muonC2_7
