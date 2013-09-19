ad64e6398a7937f134a7e756d520c8a07559578c
Pietro Vischia <pietro.vischia@gmail.com>
2013-09-19 17:09:08 +0100

    Fixed monitor script for number of running/total batch jobs

--- scripts/lip-batch/monitor.sh
+++ scripts/lip-batch/monitor.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
+# "grep vischia" selects only the lines corresponding to a job. It might as well be "grep job-" or stuff like that.
 while true ;
  do
-  echo "running: " `qstat -u vischia | grep " r " | wc -l` "    total: " `qstat -u vischia | wc -l` 
+  echo "running: " `qstat -u vischia | grep vischia | grep " r " | wc -l` "    total: " `qstat -u vischia | grep vischia | wc -l` 
 done
