e890365ecb29fd8b6f4d9c161f706c837715cc5e
Pietro Vischia <pietro.vischia@gmail.com>
2013-10-15 17:54:54 +0100

    Added H->hh->tautaubb samples to combineResults.sh

--- scripts/lip-batch/combineResults.sh
+++ scripts/lip-batch/combineResults.sh
@@ -15,7 +15,8 @@ if [ "${2}" = "hadd" ]; then
 #    elif ["${3}" = "ABC" ]; then
 #        hadd -f out-data.root out-Muon_RunA.root out-Muon_RunA_06AugRecover.root out-Muon_RunB_*.root out-Muon_RunC1.root out-Muon_RunC2_*.root
 #    elif ["${3}" = "ABCD" ]; then
-    hadd -f out-data.root out-Muon_RunA_*.root out-Muon_RunB_*.root out-Muon_RunC_1_*.root out-Muon_RunC_2_*.root out-Muon_RunD_1_*.root out-Muon_RunD_2_*.root &
+ #   hadd -f out-data.root out-Muon_RunA_*.root out-Muon_RunB_*.root out-Muon_RunC_1_*.root out-Muon_RunC_2_*.root out-Muon_RunD_1_*.root out-Muon_RunD_2_*.root &
+###    hadd -f out-data.root out-Muon_RunB_*.root out-Muon_RunC_1_*.root out-Muon_RunC_2_*.root out-Muon_RunD_1_*.root out-Muon_RunD_2_*.root &
 #    fi
 
 
@@ -25,82 +26,87 @@ if [ "${2}" = "hadd" ]; then
     cd $DATAPLACE
 
     
-    # QCD
-    hadd -f $DATAPLACE/out-qcd.root $DATAPLACE/out-qcd_*root $DATAPLACE/out-qcdmu15*.root $DATAPLACE/out-PhotonJets*root
+###    # QCD
+###    hadd -f $DATAPLACE/out-qcd.root $DATAPLACE/out-qcd_*root $DATAPLACE/out-qcdmu15*.root $DATAPLACE/out-PhotonJets*root
+###
+###    # zjets
+###    hadd -f $DATAPLACE/out-zjets.root $DATAPLACE/out-dy_from50_*root $DATAPLACE/out-dy_10to50_*.root
+###
+###    # single top
+###     hadd -f $DATAPLACE/out-singletop.root $DATAPLACE/out-stop*.root
+###    
+###    # diboson
+###    hadd -f $DATAPLACE/out-dibosons.root $DATAPLACE/out-ww_*.root $DATAPLACE/out-wz_*.root $DATAPLACE/out-zz_*.root
+###
+###    # wjets
+###     hadd -f $DATAPLACE/out-wjets.root $DATAPLACE/out-wjets_*.root
+### 
+###     hadd -f $DATAPLACE/out-ttbar.root $DATAPLACE/out-ttbar_*.root
+###     hadd -f $DATAPLACE/out-ttbar-ddbkg.root $DATAPLACE/out-ttbar-ddbkg_*.root
+###     hadd -f $DATAPLACE/out-ttbar-mcbkg.root $DATAPLACE/out-ttbar-mcbkg_*.root
+###     hadd -f $DATAPLACE/out-ttbar-mutau.root $DATAPLACE/out-ttbar-mutau_*.root
+###     hadd -f $DATAPLACE/out-ttbar-mumu.root $DATAPLACE/out-ttbar-mumu_*.root
+###     hadd -f $DATAPLACE/out-ttbar-emu.root $DATAPLACE/out-ttbar-emu_*.root
+###
+###
+###    # misidentified tau_h component from MC
+###    hadd -f $DATAPLACE/out-misidbkg.root $DATAPLACE/out-wjets.root $DATAPLACE/out-ttbar-ddbkg.root $DATAPLACE/out-qcd.root
+###
+###    
+###    # mc total
+###    hadd -f $DATAPLACE/out-mc.root $DATAPLACE/out-qcd.root $DATAPLACE/out-wjets.root $DATAPLACE/out-zjets.root $DATAPLACE/out-dibosons.root $DATAPLACE/out-ttbar.root $DATAPLACE/out-singletop.root &
+###    
+###    # data driven bkg, using mc
+###    hadd -f $DATAPLACE/out-ddbkg.root $DATAPLACE/out-qcd.root $DATAPLACE/out-wjets.root $DATAPLACE/out-ttbar-ddbkg.root
+###
+###    ## no ##    # ttbar mc for shapes?
+###    ## no ##    hadd -f $DATAPLACE/out-ttbar_mcbkg_shapes.root $DATAPLACE/out-ttbar_mcbkg.root $DATAPLACE/out-ttbar_emu.root $DATAPLACE/out-ttbar_mumu.root
+###
+###    # mc bkg (w.r.t. top xsec measurement: ttbar is signal for this)
+###    hadd -f $DATAPLACE/out-mcbkg.root $DATAPLACE/out-zjets.root $DATAPLACE/out-singletop.root $DATAPLACE/out-dibosons.root $DATAPLACE/out-ttbar-mcbkg.root
+###    ## no ## $DATAPLACE/out-ttbar_emu.root $DATAPLACE/out-ttbar_mumu.root
+###
+###    # other cont
+###    hadd -f $DATAPLACE/out-ttbar-other.root  $DATAPLACE/out-ttbar-ddbkg.root $DATAPLACE/out-ttbar-mcbkg.root &
+###
+###    # allmcmkg (w.r.t. charged higgs search: ttbar is includes as irreducible background)
+###    hadd -f $DATAPLACE/out-allmcbkg.root $DATAPLACE/out-ttbar.root $DATAPLACE/out-mcbkg.root &
+###
+###
+###    hadd -f $DATAPLACE/out-htb-pythia-m180.root $DATAPLACE/out-htb-pythia-m180_*.root 
+###    hadd -f $DATAPLACE/out-htb-pythia-m200.root $DATAPLACE/out-htb-pythia-m200_*.root 
+###    hadd -f $DATAPLACE/out-htb-pythia-m220.root $DATAPLACE/out-htb-pythia-m220_*.root 
+###    hadd -f $DATAPLACE/out-htb-pythia-m240.root $DATAPLACE/out-htb-pythia-m240_*.root 
+###    hadd -f $DATAPLACE/out-htb-pythia-m250.root $DATAPLACE/out-htb-pythia-m250_*.root 
+###    hadd -f $DATAPLACE/out-htb-pythia-m260.root $DATAPLACE/out-htb-pythia-m260_*.root 
+###    hadd -f $DATAPLACE/out-htb-pythia-m280.root $DATAPLACE/out-htb-pythia-m280_*.root 
+###    hadd -f $DATAPLACE/out-htb-pythia-m300.root $DATAPLACE/out-htb-pythia-m300_*.root 
+###    hadd -f $DATAPLACE/out-htb-pythia-m350.root $DATAPLACE/out-htb-pythia-m350_*.root 
+###    hadd -f $DATAPLACE/out-htb-pythia-m400.root $DATAPLACE/out-htb-pythia-m400_*.root 
+###    hadd -f $DATAPLACE/out-htb-pythia-m500.root $DATAPLACE/out-htb-pythia-m500_*.root 
+###    hadd -f $DATAPLACE/out-htb-pythia-m600.root $DATAPLACE/out-htb-pythia-m600_*.root 
+###    hadd -f $DATAPLACE/out-htb-pythia-m700.root $DATAPLACE/out-htb-pythia-m700_*.root 
 
-    # zjets
-    hadd -f $DATAPLACE/out-zjets.root $DATAPLACE/out-dy_from50_*root $DATAPLACE/out-dy_10to50_*.root
+    hadd -f $DATAPLACE/out-hhhtautaubb-pythia-m260.root $DATAPLACE/out-hhhtautaubb-pythia-m260_*.root 
+    hadd -f $DATAPLACE/out-hhhtautaubb-pythia-m300.root $DATAPLACE/out-hhhtautaubb-pythia-m300_*.root 
+    hadd -f $DATAPLACE/out-hhhtautaubb-pythia-m350.root $DATAPLACE/out-hhhtautaubb-pythia-m350_*.root 
 
-    # single top
-     hadd -f $DATAPLACE/out-singletop.root $DATAPLACE/out-stop*.root
-    
-    # diboson
-    hadd -f $DATAPLACE/out-dibosons.root $DATAPLACE/out-ww_*.root $DATAPLACE/out-wz_*.root $DATAPLACE/out-zz_*.root
-
-    # wjets
-     hadd -f $DATAPLACE/out-wjets.root $DATAPLACE/out-wjets_*.root
- 
-     hadd -f $DATAPLACE/out-ttbar.root $DATAPLACE/out-ttbar_*.root
-     hadd -f $DATAPLACE/out-ttbar-ddbkg.root $DATAPLACE/out-ttbar-ddbkg_*.root
-     hadd -f $DATAPLACE/out-ttbar-mcbkg.root $DATAPLACE/out-ttbar-mcbkg_*.root
-     hadd -f $DATAPLACE/out-ttbar-mutau.root $DATAPLACE/out-ttbar-mutau_*.root
-     hadd -f $DATAPLACE/out-ttbar-mumu.root $DATAPLACE/out-ttbar-mumu_*.root
-     hadd -f $DATAPLACE/out-ttbar-emu.root $DATAPLACE/out-ttbar-emu_*.root
-
-
-    # misidentified tau_h component from MC
-    hadd -f $DATAPLACE/out-misidbkg.root $DATAPLACE/out-wjets.root $DATAPLACE/out-ttbar-ddbkg.root $DATAPLACE/out-qcd.root
-
-    
-    # mc total
-    hadd -f $DATAPLACE/out-mc.root $DATAPLACE/out-qcd.root $DATAPLACE/out-wjets.root $DATAPLACE/out-zjets.root $DATAPLACE/out-dibosons.root $DATAPLACE/out-ttbar.root $DATAPLACE/out-singletop.root &
-    
-    # data driven bkg, using mc
-    hadd -f $DATAPLACE/out-ddbkg.root $DATAPLACE/out-qcd.root $DATAPLACE/out-wjets.root $DATAPLACE/out-ttbar-ddbkg.root
-
-    ## no ##    # ttbar mc for shapes?
-    ## no ##    hadd -f $DATAPLACE/out-ttbar_mcbkg_shapes.root $DATAPLACE/out-ttbar_mcbkg.root $DATAPLACE/out-ttbar_emu.root $DATAPLACE/out-ttbar_mumu.root
-
-    # mc bkg (w.r.t. top xsec measurement: ttbar is signal for this)
-    hadd -f $DATAPLACE/out-mcbkg.root $DATAPLACE/out-zjets.root $DATAPLACE/out-singletop.root $DATAPLACE/out-dibosons.root $DATAPLACE/out-ttbar-mcbkg.root
-    ## no ## $DATAPLACE/out-ttbar_emu.root $DATAPLACE/out-ttbar_mumu.root
-
-    # other cont
-    hadd -f $DATAPLACE/out-ttbar-other.root  $DATAPLACE/out-ttbar-ddbkg.root $DATAPLACE/out-ttbar-mcbkg.root &
-
-    # allmcmkg (w.r.t. charged higgs search: ttbar is includes as irreducible background)
-    hadd -f $DATAPLACE/out-allmcbkg.root $DATAPLACE/out-ttbar.root $DATAPLACE/out-mcbkg.root &
-
-
-    hadd -f $DATAPLACE/out-htb-pythia-m180.root $DATAPLACE/out-htb-pythia-m180_*.root 
-    hadd -f $DATAPLACE/out-htb-pythia-m200.root $DATAPLACE/out-htb-pythia-m200_*.root 
-    hadd -f $DATAPLACE/out-htb-pythia-m220.root $DATAPLACE/out-htb-pythia-m220_*.root 
-    hadd -f $DATAPLACE/out-htb-pythia-m240.root $DATAPLACE/out-htb-pythia-m240_*.root 
-    hadd -f $DATAPLACE/out-htb-pythia-m250.root $DATAPLACE/out-htb-pythia-m250_*.root 
-    hadd -f $DATAPLACE/out-htb-pythia-m260.root $DATAPLACE/out-htb-pythia-m260_*.root 
-    hadd -f $DATAPLACE/out-htb-pythia-m280.root $DATAPLACE/out-htb-pythia-m280_*.root 
-    hadd -f $DATAPLACE/out-htb-pythia-m300.root $DATAPLACE/out-htb-pythia-m300_*.root 
-    hadd -f $DATAPLACE/out-htb-pythia-m350.root $DATAPLACE/out-htb-pythia-m350_*.root 
-    hadd -f $DATAPLACE/out-htb-pythia-m400.root $DATAPLACE/out-htb-pythia-m400_*.root 
-    hadd -f $DATAPLACE/out-htb-pythia-m500.root $DATAPLACE/out-htb-pythia-m500_*.root 
-    hadd -f $DATAPLACE/out-htb-pythia-m600.root $DATAPLACE/out-htb-pythia-m600_*.root 
-    hadd -f $DATAPLACE/out-htb-pythia-m700.root $DATAPLACE/out-htb-pythia-m700_*.root 
-
-    # higgs  mass points
-    hadd -f $DATAPLACE/out-heavyHiggs-pythia-m180.root $DATAPLACE/out-tbh-pythia-m180.root $DATAPLACE/out-htb-pythia-m180.root &
-    cp  $DATAPLACE/out-tbh-pythia-m190.root $DATAPLACE/out-heavyHiggs-pythia-m190.root                                         & 
-    hadd -f $DATAPLACE/out-heavyHiggs-pythia-m200.root $DATAPLACE/out-tbh-pythia-m200.root $DATAPLACE/out-htb-pythia-m200.root &
-    hadd -f $DATAPLACE/out-heavyHiggs-pythia-m220.root $DATAPLACE/out-tbh-pythia-m220.root $DATAPLACE/out-htb-pythia-m220.root &
-    cp  $DATAPLACE/out-htb-pythia-m240.root $DATAPLACE/out-heavyHiggs-pythia-m240.root                                         &  
-    hadd -f $DATAPLACE/out-heavyHiggs-pythia-m250.root $DATAPLACE/out-tbh-pythia-m250.root $DATAPLACE/out-htb-pythia-m250.root &
-    cp  $DATAPLACE/out-htb-pythia-m260.root $DATAPLACE/out-heavyHiggs-pythia-m260.root                                         &
-    cp  $DATAPLACE/out-htb-pythia-m280.root $DATAPLACE/out-heavyHiggs-pythia-m280.root                                         &
-    hadd -f $DATAPLACE/out-heavyHiggs-pythia-m300.root $DATAPLACE/out-tbh-pythia-m300.root $DATAPLACE/out-htb-pythia-m300.root &
-    cp  $DATAPLACE/out-htb-pythia-m350.root $DATAPLACE/out-heavyHiggs-pythia-m350.root                                         &
-    cp  $DATAPLACE/out-htb-pythia-m400.root $DATAPLACE/out-heavyHiggs-pythia-m400.root                                         &
-    cp  $DATAPLACE/out-htb-pythia-m500.root $DATAPLACE/out-heavyHiggs-pythia-m500.root                                         &
-    cp  $DATAPLACE/out-htb-pythia-m600.root $DATAPLACE/out-heavyHiggs-pythia-m600.root                                         &
-    cp  $DATAPLACE/out-htb-pythia-m700.root $DATAPLACE/out-heavyHiggs-pythia-m700.root                                         &
+###
+###    # higgs  mass points
+###    hadd -f $DATAPLACE/out-heavyHiggs-pythia-m180.root $DATAPLACE/out-tbh-pythia-m180.root $DATAPLACE/out-htb-pythia-m180.root &
+###    cp  $DATAPLACE/out-tbh-pythia-m190.root $DATAPLACE/out-heavyHiggs-pythia-m190.root                                         & 
+###    hadd -f $DATAPLACE/out-heavyHiggs-pythia-m200.root $DATAPLACE/out-tbh-pythia-m200.root $DATAPLACE/out-htb-pythia-m200.root &
+###    hadd -f $DATAPLACE/out-heavyHiggs-pythia-m220.root $DATAPLACE/out-tbh-pythia-m220.root $DATAPLACE/out-htb-pythia-m220.root &
+###    cp  $DATAPLACE/out-htb-pythia-m240.root $DATAPLACE/out-heavyHiggs-pythia-m240.root                                         &  
+###    hadd -f $DATAPLACE/out-heavyHiggs-pythia-m250.root $DATAPLACE/out-tbh-pythia-m250.root $DATAPLACE/out-htb-pythia-m250.root &
+###    cp  $DATAPLACE/out-htb-pythia-m260.root $DATAPLACE/out-heavyHiggs-pythia-m260.root                                         &
+###    cp  $DATAPLACE/out-htb-pythia-m280.root $DATAPLACE/out-heavyHiggs-pythia-m280.root                                         &
+###    hadd -f $DATAPLACE/out-heavyHiggs-pythia-m300.root $DATAPLACE/out-tbh-pythia-m300.root $DATAPLACE/out-htb-pythia-m300.root &
+###    cp  $DATAPLACE/out-htb-pythia-m350.root $DATAPLACE/out-heavyHiggs-pythia-m350.root                                         &
+###    cp  $DATAPLACE/out-htb-pythia-m400.root $DATAPLACE/out-heavyHiggs-pythia-m400.root                                         &
+###    cp  $DATAPLACE/out-htb-pythia-m500.root $DATAPLACE/out-heavyHiggs-pythia-m500.root                                         &
+###    cp  $DATAPLACE/out-htb-pythia-m600.root $DATAPLACE/out-heavyHiggs-pythia-m600.root                                         &
+###    cp  $DATAPLACE/out-htb-pythia-m700.root $DATAPLACE/out-heavyHiggs-pythia-m700.root                                         &
 
 
 elif [ "${2}" = "clean" ]; then
