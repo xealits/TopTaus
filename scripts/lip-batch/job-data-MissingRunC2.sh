1ca9fa929fef918e850e10275a57c40dce69c951
Pietro Vischia <pietro.vischia@gmail.com>
2013-12-18 19:38:38 +0000

    Submission scripts - forgot to commit them before

---
+++ scripts/lip-batch/job-data-MissingRunC2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+. $LIPCMS_BASE/scripts/lip-batch/run-batch.sh
+
+physicsAnalysis $LIPCMS_BASE/test/physicsAnalysisParSets_cfg.py data_missingmuonC2_${1}
---
+++ scripts/lip-batch/job-data-MissingRunD1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+. $LIPCMS_BASE/scripts/lip-batch/run-batch.sh
+
+physicsAnalysis $LIPCMS_BASE/test/physicsAnalysisParSets_cfg.py data_missingmuonD1_${1}
